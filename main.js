eebcade2f9485c53f134fcfe6884d5b1b1ee98ed
Jill Hubbard <jill.m.hubbard@gmail.com>
2019-11-20 15:15:45 -0800

    Exercise 15

--- main.js
+++ main.js
@@ -1,8 +1,26 @@
+// $(updateView)
+// function updateView() {
+//     alert("It's me, Mario!")
+// }
+
+// $.getJSON("https://zagster-service.herokuapp.com/rides/count", function(data) {
+//     alert(JSON.stringify(data));
+//     //alert(data.count);
+//     console.log(data);
+// });
+
+
+const BASE_URL = "https://zagster-service.herokuapp.com"
+
 $(updateView)
+
 function updateView() {
-    alert("It's me, Mario!")
+  $.getJSON(BASE_URL + "/rides/count" , updateRideCount)
 }
 
-
-
-
+function updateRideCount(data) {
+  numberOfRides = data.count
+  $("h2#rideCount").html(numberOfRides)
+  alert(numberOfRides);
+  console.log(numberOfRides);
+}
